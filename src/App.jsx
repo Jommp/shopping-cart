276d5b4e67e6559ab618ced56caae3b21de131d3
Jommp <jommp.dev@gmail.com>
2025-08-18 12:20:05 -0500

    feat(App.jsx): implement mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 
+import db from "./mock/db.json";
+
 function App() {
+  const [guitars, setGuitars] = useState(db);
+
   return (
     <>
       <Header />
